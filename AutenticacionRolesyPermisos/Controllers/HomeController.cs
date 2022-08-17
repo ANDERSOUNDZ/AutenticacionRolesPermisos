ae8a7ca0523e8ec1a4ba28b87d6abea27f0eeccf
ANDERSOUNDZ <andersonmikol@live.com>
2022-08-16 23:09:10 -0500

    Agregando proyecto con Vistas controladores iniciales, 13:06

---
+++ AutenticacionRolesyPermisos/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using AutenticacionRolesyPermisos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AutenticacionRolesyPermisos.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Ventas()
+        {
+            return View();
+        }
+
+        public IActionResult Compras()
+        {
+            return View();
+        }
+
+        public IActionResult Clientes()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
