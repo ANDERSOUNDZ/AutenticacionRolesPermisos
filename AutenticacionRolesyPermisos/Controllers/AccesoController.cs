90e8ec886a59046a780d3b0ca4937fc908a4de67
ANDERSOUNDZ <andersonmikol@live.com>
2022-08-17 20:11:58 -0500

    Inicio de configuracion de Autenticacion cookie, 28:37

--- AutenticacionRolesyPermisos/Controllers/AccesoController.cs
+++ AutenticacionRolesyPermisos/Controllers/AccesoController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using AutenticacionRolesyPermisos.Models;
+using AutenticacionRolesyPermisos.Data;
 
 namespace AutenticacionRolesyPermisos.Controllers
 {
@@ -7,6 +9,26 @@ namespace AutenticacionRolesyPermisos.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Usuario _usuario)
+        {
+            DA_Logica _da_usuario = new DA_Logica();
+            var usuario = _da_usuario.ValidarUsuario(_usuario.Correo, _usuario.Clave );
+
+            if (usuario != null) {
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Salir()
+        {
+            return RedirectToAction("Index", "Acceso");
         }
     }
 }
