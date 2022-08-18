149160a5e5dea94dcaae187e8c0061b8701c1235
ANDERSOUNDZ <andersonmikol@live.com>
2022-08-17 19:54:47 -0500

    Agregando nuevo modelo, y carpeta de datos para obtener datos del usuario, 21:02

---
+++ AutenticacionRolesyPermisos/Data/DA_Logica.cs
@@ -0,0 +1,54 @@
+﻿using AutenticacionRolesyPermisos.Models;
+
+namespace AutenticacionRolesyPermisos.Data
+{
+    public class DA_Logica
+    {
+        public List<Usuario> ListaUsuarios  (){
+
+            return new List<Usuario>
+            {
+                new Usuario { 
+                    Nombre = "Anderson",
+                    Correo = "admin@mail.com",
+                    Clave = "12345",
+                    Roles = new string[]{
+                    "Administracion"
+                    }
+                },
+                new Usuario {
+                    Nombre = "Maria",
+                    Correo = "maria@mail.com",
+                    Clave = "12345",
+                    Roles = new string[]{
+                    "Supervisor"
+                    }
+                },
+                new Usuario {
+                    Nombre = "Nicolas",
+                    Correo = "nicolas@mail.com",
+                    Clave = "12345",
+                    Roles = new string[]{
+                    "Empleado"
+                    }
+                },
+                new Usuario {
+                    Nombre = "Brandon",
+                    Correo = "brandon@mail.com",
+                    Clave = "12345",
+                    Roles = new string[]{
+                    "Supervisor", "Empleado"
+                    }
+                }
+            };
+        }
+
+        public Usuario ValidarUsuario(string _correo, string _clave) {
+            return ListaUsuarios().Where(item => item.Correo == _correo && item.Clave==_clave).FirstOrDefault();
+        }
+
+
+
+
+    }
+}
---
+++ AutenticacionRolesyPermisos/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿namespace AutenticacionRolesyPermisos.Models
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+
+        //Roles de usuario
+        public string[] Roles { get; set; }
+    }
+
+
+
+
+}
